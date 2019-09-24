22423bf854ee0f7d4162300e0b3d5803c554b0c0
wcopeland118 <wcopeland118@gmail.com>
2019-09-23 19:47:57 -0500

    forgot to commit and push

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,25 @@
-// from data.js
-var tableData = data;
+// var data defined in data.js file
 
-// YOUR CODE HERE!
+let tbody = d3.select("tbody");
+
+function tablemaker(data) {
+  data.forEach(d => {
+    let row = tbody.append("tr");
+    Object.entries(d).forEach(([key, value]) => {
+      let cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+tablemaker(data);
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+  tbody.html("");
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  var filteredData = data.filter(d => d.datetime === inputValue);
+  tablemaker(filteredData);
+});
