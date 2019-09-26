e7f71436210952cad59c4e7c9afbef683c049256
wcopeland118 <wcopeland118@gmail.com>
2019-09-26 12:07:50 -0500

    Added UFO 2 stuff

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,7 @@
 // var data defined in data.js file
 
+let tableData = data;
+
 let tbody = d3.select("tbody");
 
 function tablemaker(data) {
@@ -12,14 +14,35 @@ function tablemaker(data) {
   });
 }
 
-tablemaker(data);
+tablemaker(tableData);
 
-var button = d3.select("#filter-btn");
+let button = d3.select("#filter-btn");
 
 button.on("click", function() {
+  d3.event.preventDefault();
   tbody.html("");
-  var inputElement = d3.select("#datetime");
-  var inputValue = inputElement.property("value");
-  var filteredData = data.filter(d => d.datetime === inputValue);
+  let date = d3.select("#datetime").property("value");
+  let state = d3.select("#state").property("value");
+  let city = d3.select("#city").property("value");
+  let country = d3.select("#country").property("value");
+  let shape = d3.select("#shape").property("value");
+
+  let filteredData = tableData;
+
+  if (date) {
+    filteredData = filteredData.filter(d => d.datetime === date);
+  }
+  if (state) {
+    filteredData = filteredData.filter(d => d.state === state);
+  }
+  if (city) {
+    filteredData = filteredData.filter(d => d.city === city);
+  }
+  if (country) {
+    filteredData = filteredData.filter(d => d.country === country);
+  }
+  if (shape) {
+    filteredData = filteredData.filter(d => d.shape === shape);
+  }
   tablemaker(filteredData);
 });
